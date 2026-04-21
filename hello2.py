ecb150d3a96224d8b1a3bf56df8da10ec797bd50
fasaraujo <netservicero@gmail.com>
2026-04-21 17:04:13 -0400

    Delete hello.py e alerações no codigo

--- hello2.py
+++ hello2.py
@@ -1,2 +1,17 @@
-print('Arquivo 2 adicionado alt codigo')
-print('Atualido')
+def palavra(s1):
+    min = 5
+    max = 10
+    if (len(s1) >= min and len(s1) <= max):
+        print('Caiu no True')
+        return True
+    else:
+        print('Caiu no False')
+        return False
+    
+
+x = 'Fra'
+palavra(x)
+
+# na bruch nova_feature nao temos mais o arq hello.py
+
+print('Nova Feture Adicionada...')
